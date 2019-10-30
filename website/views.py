5c504e1101907862ecd44f4de31b2b5d30c7d1cb
Apamela <pamelab.desire@gmail.com>
2019-10-30 17:52:21 +0200

    add the navbar color

--- website/views.py
+++ website/views.py
@@ -133,15 +133,10 @@ def new_project(request):
     return render(request, 'new_project.html', {"form": form})
 
 # Viewing a single picture
-@login_required(login_url='/accounts/login/')
-def user_list(request):
-    user_list = User.objects.all()
-    context = {'user_list': user_list}
-    return render(request, 'user_list.html', context)
 
 
 @login_required(login_url='/accounts/login/')
-def profile(request):
+def profile(request,username=None):
     current_user = request.user
 
     if request.method == 'POST':
